6a2f9445e1ca6ac637fdb44071fb69e834f45e13
Unknown <azettler@live.com>
2018-05-10 15:44:10 -0400

    Everything is better Classy
    Rewrote in a more object oriented fashion

--- source/workspace_maker.py
+++ source/workspace_maker.py
@@ -1,10 +1,39 @@
 import json
 
-def create_workspace(proj_path: str, proj_name: str, python_path: str):
+def create_workspace(proj_path: str, proj_name: str):
     
-	#print("project dir: {}\nfolder name: {}".format(proj_path, proj_name))
+	#Make the workspace dictionary
+	vs_code_dict = make_workspace_dict()
 
-	#Populate our dictionary to write
+	#Create the json file to write
+	json_code_str = json.dumps(vs_code_dict, sort_keys=True, indent=2, separators=(',', ': '))
+
+	#Create the path including the file name
+	file_path_and_name = get_workspace_path(proj_path, proj_name)
+
+	# Write json data
+	with open(file_path_and_name,"w") as f:
+		f.write(json_code_str)
+
+def get_workspace_name(proj_name:str):
+	"""
+	returns the name of the workspace file to create
+	"""
+	return "{}.code-workspace".format(proj_name)
+
+def get_workspace_path(proj_path:str, proj_name:str):
+	"""
+	returns the path and name of the workspace file to create
+	"""
+	return "{}/{}".format(
+	proj_path, 
+	get_workspace_name(proj_name)
+	)
+
+def make_workspace_dict():
+	"""
+	returns the populated vscode dictionary to write
+	"""
 	vs_code_dict = {
 		"folders": [{
 			"path": "source"
@@ -14,16 +43,4 @@ def create_workspace(proj_path: str, proj_name: str, python_path: str):
 			#"python.pythonPath": python_path
 		}
 	}
-
-	#Create the json file to write
-	json_code_str = json.dumps(vs_code_dict, sort_keys=True, indent=2, separators=(',', ': '))
-
-	#Create the path including the file name
-	file_path_and_name = "{}/{}.code-workspace".format(
-		proj_path, 
-		proj_name
-		)
-
-	# Write json data
-	with open(file_path_and_name,"w") as f:
-		f.write(json_code_str)
+	return vs_code_dict
