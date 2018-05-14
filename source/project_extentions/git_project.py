2d42cd6da1b9078ea368daa5343cbb9e297d78a9
Unknown <azettler@live.com>
2018-05-14 13:05:14 -0400

    Fixed git check for existing to require a .git directory extention

--- source/project_extentions/git_project.py
+++ source/project_extentions/git_project.py
@@ -12,12 +12,13 @@ class GitProject(ProjectExtention):
     def _create(self):
         print("One day I will be able to create your glorious git repo")
         # git.create_git_repo(proj_path)
+        raise NotImplementedError
 
     def _load(self):
         pass
 
     def _open(self):
-        pass
+        raise UnopenableProject
 
     def _check_for_existing(self):
         """
@@ -25,10 +26,14 @@ class GitProject(ProjectExtention):
         """
         os.chdir(self.proj_path)
 
+        #return True in [f for f in os.listdir() if os.path.isdir(f) and f == ".git"]
+
         for f in os.listdir():
             if os.path.isdir(f):
-                print(f)
-        return True
+                if f == ".git":
+                    return True
+        return False
+
 
     def _on_existing_create_new(self):
         return False
